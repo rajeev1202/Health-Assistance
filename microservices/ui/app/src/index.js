dae5d6931fbba5047a4b03b4580432dd3173814f
rajeev ranjan <talegarry1202@gmail.com>
2018-02-04 01:10:34 +0530

    second commit

--- microservices/ui/app/src/index.js
+++ microservices/ui/app/src/index.js
@@ -2,15 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {HomePage} from './HomePage';
-import {TabsExampleIconText} from './TabsExampleControlled'
-import {Feed} from './feed.js';
-import {TwitterAdvetise} from './TwitterAdvetise.js'
-import {WhoToFollow} from './WhoToFollow.js';
-import {TrendsForYou} from './TrendsForYou.js';
-import {SearchPage} from './SearchPage.js';
+//import {HomePage} from './HomePage';
+//import {TabsExampleIconText} from './TabsExampleControlled'
+//import {Feed} from './feed.js';
+//import {TwitterAdvetise} from './TwitterAdvetise.js'
+//import {WhoToFollow} from './WhoToFollow.js';
+//import {TrendsForYou} from './TrendsForYou.js';
+//import {SearchPage} from './SearchPage.js';
 import {QaHome} from './QaHome'
-import {Dummy} from './Dummy'
+//import {Dummy} from './Dummy'
 document.body.style = 'background:rgb(210, 220, 237)';
 
 class App extends  React.Component{
